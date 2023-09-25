b73934b64daa6afc80031aeaa04bdeb38167aebe
brunoferreira182 <brunoferreira1821995@gmail.com>
2023-09-25 17:50:02 -0300

    inicinado checkbox das permissionsa dentro de userDetail

--- src/pages/users/UserDetail.vue
+++ src/pages/users/UserDetail.vue
@@ -8,26 +8,58 @@
         </div>
         <div class="col q-gutter-sm text-right">
           <q-chip
+            v-if="userData.status"
             color="green-8"
             outline
-            size="md"
+            size="medium"
           >
             {{ userData.status.label }}
           </q-chip>
         </div>
       </div>
       <q-separator class="q-mx-md"></q-separator>
-      <div class="row justify-center items-start">
-        <div class="col-12 q-pa-md q-gutter-md">
-          <div class="text-h4">Informações</div>
-          <div class="text-h5">
-            E-mail: {{ userData.email }}
+      <div class="row justify-between items-start">
+        <div class="col-6 q-pa-md q-gutter-lg">
+          <div class="text-grey-8 text-h6">
+            Informações
           </div>
-          <div class="text-h5">
-            Telefone: {{ userData.phone }}
+          <q-input
+            outlined
+            readonly
+            v-model="userData.email"
+            label="E-mail"
+          />
+          <q-input
+            outlined
+            readonly
+            v-model="userData.phone"
+            label="Telefone"
+          />
+          <q-input
+            outlined
+            readonly
+            v-model="userData.user "
+            label="Usuário"
+          />
+        </div>
+        <q-separator vertical />
+        <div class="col-6 q-pa-md q-gutter-md">
+          <div class="text-grey-8 text-h6 q-px-xs">Permissões:</div>
+          <div class="text-caption text-grey-8 q-ma-none q-mt-sm q-px-md">
+            Gerencie as permissões deste usuário
           </div>
-          <div class="text-h5">
-            Usuário: {{ userData.user }}
+          <div class="visions-field q-mt-none row">
+            <div
+              v-for="permission in allPermissions"
+              :key="permission"
+              class="col-12 q-my-xs"
+            >
+              <q-checkbox 
+                @update:model-value="(value, evt ) => updateUserPermissions(value, evt, permission)"
+                :label="permission.label"
+                v-model="permission.checked"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -42,6 +74,11 @@ export default defineComponent({
   data() {
     return {
       userData: {},
+      permissions:[],
+      allPermissions: [],
+      checkedPermissionsList: [],
+      userIdSQL: '',
+      isActive: 0,
     }
   },
   mounted() {
@@ -51,6 +88,29 @@ export default defineComponent({
     this.getUserDetailById()
   },
   methods: {
+    updateUserPermissions(value, evt, permission) {
+      console.log('permission', permission)
+      console.log('value', value[0])
+      console.log('evt', evt)
+      const isActive = value[0] ? true : false
+      const opt = {
+        route: "/desktop/users/updateUserPermissions",
+        body: {
+          userId: this.userIdSQL,
+          permissionId: permission.id,
+          isActive
+        }
+      };
+      this.$q.loading.show()
+      useFetch(opt).then((r) => {
+        this.$q.loading.hide()
+        if (r.error) {
+          this.$q.notify("Ocorreu um erro, tente novamente por favor");
+        } else {
+          this.getUserDetailById()
+        }
+      });
+    },
     getUserDetailById() {
       const opt = {
         route: "/desktop/users/getUserDetailById",
@@ -59,10 +119,34 @@ export default defineComponent({
         }
       };
       useFetch(opt).then((r) => {
-        this.userData = r.data
+        if (r.error) {
+          this.$q.notify("Ocorreu um erro, tente novamente por favor");
+        } else {
+          this.userData = r.data
+          this.allPermissions = r.data.allPermissions,
+          this.userIdSQL = r.data.userId
+          this.checkedPermissionsList = r.data.permissions
+          // this.allPermissions.forEach((all, i) => {this.allPermissions[i].checked = false;})
+          this.allPermissions.forEach((all, i) => {
+            this.checkedPermissionsList.forEach((checked) => {
+              if (all.id === checked.id) {
+                this.allPermissions[i].checked = true;
+              }
+            });
+          });
+        }
       });
     },
   }
 })
 </script>
+<style scoped>
+.visions-field {
+  border: 1px solid #c2c2c2;
+  padding: 7px;
+  border-radius: 0.2rem;
+  max-height: 300px;
+  overflow-y: auto;
+}
+</style>
 
