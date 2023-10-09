2687f8157579611bf9a4f971aa4e0f310e08e4e8
brunoferreira182 <brunoferreira1821995@gmail.com>
2023-10-09 16:36:02 -0300

    ajuste

--- src/boot/masterServerRoutes.js
+++ src/boot/masterServerRoutes.js
@@ -7,7 +7,7 @@ function masterServerRoute () {
   if (MODE_MASTER_SERVER === 'local') ret = 'http://localhost:' + port
   else if (MODE_MASTER_SERVER === 'dev') ret = 'https://api6.b3dev.dev:' + port
   else if (MODE_MASTER_SERVER === 'tiago') ret = 'http://192.168.1.3:' + port
-  else if (MODE_MASTER_SERVER === 'darta') ret = 'http://192.168.1.5:' + port
+  else if (MODE_MASTER_SERVER === 'darta') ret = 'http://192.168.1.2:' + port
   else if (MODE_MASTER_SERVER === 'prod') ret = 'https://api2.b3dev.dev:' + port
   return ret
 }
